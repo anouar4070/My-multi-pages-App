601a7ca0dfd5e42f741b4f5e1b3e45182f507ed4
anouar garbaa <g.anouar@yahoo.com>
2024-10-28 21:16:39 +0100

    add relative paths

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -23,8 +24,9 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
+      { path: "", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function HomePage() {
     <>
       <h1> Home page </h1>
       <p>
-        Go to <Link to="/products">the list of products</Link>
+        Go to <Link to="products">the list of products</Link>
       </p>
       <p>
         <button onClick={navigateHandler}>Navigate</button>
